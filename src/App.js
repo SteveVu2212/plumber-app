70c821257e3678696b00a8549fd98c01296c1c14
Anh Tu Vu <anhtu@DevBook.local>
2023-05-17 22:01:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  // Define state variables to hold input and output data
+  const [petalWidth, setPetalWidth] = useState(0);
+  const [predictedPetalLength, setPredictedPetalLength] = useState(0);
+
+  // Define a function to handle the form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const apiUrl = 'http://206.81.14.205:80';
+      const endpoint = '/predict_petal_length';
+      const url = `${apiUrl}${endpoint}?petal_width=${petalWidth}`;
+      const response = await axios.get(url);
+      
+      setPredictedPetalLength(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Display an error message to the user
+      alert('Error fetching data. Please try again later.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Petal Length Predictor</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Petal Width:
+          <input
+            type="number"
+            value={petalWidth}
+            onChange={(event) => setPetalWidth(event.target.value)}
+          />
+        </label>
+        <button type="submit">Predict Petal Length</button>
+      </form>
+      <p>Predicted Petal Length: {predictedPetalLength}</p>
     </div>
   );
 }
